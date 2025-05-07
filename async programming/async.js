5139d4211bd25f803ff335d273099f13fecf1834
ARYAN242526 <roy2503aryan@gmail.com>
2025-05-07 23:55:02 +0530

    asynchronous programming

---
+++ async programming/async.js
@@ -0,0 +1,21 @@
+// console.log('hello');
+
+// function greeting(){
+//     console.log('I am from setTimeout');
+// }
+
+// setTimeout(greeting,3000);
+
+// console.log('Javascript');
+
+
+console.log('Start');
+
+setTimeout(() => {
+    console.log('I am from setTimeout');
+    
+
+},2000)
+
+console.log('End');
+
